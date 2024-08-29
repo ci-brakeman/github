7f36e591cd8e8f1e7e525ead9fac55d50edd9f56
root <root@server.localhost.com>
2024-08-29 07:30:52 +0200

    Initial commit

---
+++ github.go
@@ -0,0 +1,15 @@
+// github.go
+package github
+
+import (
+	"fmt"
+)
+
+// JWTToken is a global variable to store the JWT token
+var JWTToken string
+
+// GetAccessToken is a mock function that prints "anupamas0x1"
+func GetAccessToken(installationID string) error {
+	fmt.Println("anupamas0x1")
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ci-brakeman/github
+
+go 1.13
